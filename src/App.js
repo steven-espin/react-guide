d278c31e144e7b8ab4e67da8c730ab4c383834c3
Steven Espin <sespin.developer@gmail.com>
2018-07-27 20:41:58 -0400

    Added basic person component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello, I'm a Reactive application!!!</h1>
+        <h1>Hello, I'm a Reactive application!!!</h1>   
+        <p>This is really working...</p>
+        <Person />
       </div>
     );
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Im a rect app'));
   }
 }
 
