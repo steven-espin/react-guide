cf7638cfa8a6e2d692ee77eebde37dc8f499a1b1
Steven Espin <sespin.developer@gmail.com>
2018-07-29 12:17:36 -0400

    Changing state through setState() and clickHandler

--- src/App.js
+++ src/App.js
@@ -3,14 +3,38 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Steven', age: '22' },
+      { name: 'Kevin', age: '23' },
+      { name: 'Bob', age: '25' }
+    ]
+  }
+
+  swtchNameHandler = () => {
+    console.log("clicked...");
+    // This won't work to change state: this.state.persons[0].name = 'Max';
+    // setState function required to refresh DOM once state is changed.
+    // only 2 things refresh DOM, changing state and props
+  
+    this.setState( {
+      persons: [
+        { name: 'Max', age: '27' },
+        { name: 'Kevin', age: '27' },
+        { name: 'Bobby', age: '27' }
+      ]
+    } )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello, I'm a Reactive application!!!</h1>   
         <p>This is really working...</p>
-        <Person name='Steven' age='22'/>
-        <Person name='Kevin' age='23'>my hobbies: motorcycling</Person>
-        <Person name='Bob' age='25'/>
+        <button onClick={this.swtchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>my hobbies: motorcycling</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
