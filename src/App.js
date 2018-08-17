41ef1c4ed87cbc541048b4c7831e39434832c4f2
StevenE <sespin1996@gmail.com>
2018-08-16 23:47:14 -0700

    enabling and using CSS Modules for Media Queries and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -41,16 +41,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white', 
-      font: 'inherit',
-      border: '1px solid blue', 
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = null;
 
     if (this.state.showPersons) {
       persons = (
@@ -64,23 +56,22 @@ class App extends Component {
             })}
         </div>
       );
-
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length < 3) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length < 2) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hello, I'm a Reactive application!!!</h1>   
-        <p className={classes.join(' ')}>This is really working...</p>
-        <button onClick={this.togglePersonsHandler} style={style}>Toggle Persons</button> 
+        <p className={assignedClasses.join(' ')}>This is really working...</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button> 
         {persons}
       </div>
     );
